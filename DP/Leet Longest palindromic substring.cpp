44b72ec36e8bdec165a61c400d44e27f803d593b
bharat <bharatkumarmakhija14@gmail.com>
2017-02-05 18:15:15 +0530

    modified a file

--- DP/Leet Longest palindromic substring.cpp
+++ DP/Leet Longest palindromic substring.cpp
@@ -2,17 +2,6 @@
 Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
 */
 
-// Source : https://oj.leetcode.com/problems/longest-palindromic-substring/
-// Author : Hao Chen
-// Date   : 2014-07-17
-
-/********************************************************************************** 
-* 
-* Given a string S, find the longest palindromic substring in S. 
-* You may assume that the maximum length of S is 1000, 
-* and there exists one unique longest palindromic substring.
-*               
-**********************************************************************************/
 
 #include <string.h>
 #include <iostream>
